916d5e4db06806a8ea8c515af94f71a95ac78939
henhuang <hengkuai.huang@nokia-sbell.com>
2020-02-18 23:14:34 +0800

    refactor

--- email_commander.py
+++ email_commander.py
@@ -14,28 +14,46 @@ from config import Config
 WIDTH = 50
 
 
-
-class SmtpServer(object):
+class SmtpPop3Base(object):
     def __init__(self, configurator):
         self.closed = True
         self.config = configurator
-        self.from_addr = self.config.USERNAME
 
     def login(self):
-        self.server = smtplib.SMTP(self.config.SMTP_SERVER, self.config.SMTP_PORT)
-        self.server.set_debuglevel(self.config.DEBUG_LEVEL)
-        self.server.login(self.config.USERNAME, self.config.PASSWORD)
+        self._login_impl()
         self.closed = False
+
+    def assert_server_is_login(self, msg='Server is not login'):
+        if self.closed:
+            raise AssertionError(msg)
 
     def quit(self):
         if not self.closed:
-            self.server.quit()
+            self._quit_impl()
             self.closed = True
 
+    def _login_impl(self):
+        raise NotImplementedError # virual function
+
+    def _quit_impl(self):
+        raise NotImplementedError # virual function
+
+
+class SmtpServer(SmtpPop3Base):
+    def __init__(self, configurator):
+        super().__init__(configurator)
+        self.from_addr = configurator.USERNAME
+
+    def _login_impl(self):
+        self.server = smtplib.SMTP(self.config.SMTP_SERVER, self.config.SMTP_PORT)
+        self.server.set_debuglevel(self.config.DEBUG_LEVEL)
+        self.server.login(self.config.USERNAME, self.config.PASSWORD)
+
+    def _quit_impl(self):
+        self.server.quit()
+
     def send_msg(self, to_addr, msg_subject='', msg_body=None):
-        if self.closed:
-            print('SMTP server is not login')
-            return False
+        self.assert_server_is_login('SMTP server is not login')
         if not msg_body:
             msg_body = msg_subject or '<EMPTY>'
         msg = MIMEText(msg_body, 'plain', 'utf-8')
@@ -50,26 +68,28 @@ class SmtpServer(object):
             return False
 
 
-class PopServer(object):
+class PopServer(SmtpPop3Base):
     def __init__(self, configurator):
-        self.config = configurator
+        super().__init__(configurator)
         self.parser = Parser()
         self.login()
 
-    def login(self):
+    def _login_impl(self):
         self.server = poplib.POP3(self.config.POP_SERVER)
         self.server.set_debuglevel(self.config.DEBUG_LEVEL)
         self.server.user(self.config.USERNAME)
         self.server.pass_(self.config.PASSWORD)
 
-    def quit(self):
+    def _quit_impl(self):
         self.server.quit()
 
     @property
     def msg_num(self):
+        self.assert_server_is_login()
         return self.server.stat()[0]
 
     def get_msg(self, index):
+        self.assert_server_is_login()
         __, lines, __ = self.server.retr(index)
         msg = b'\n'.join(lines).decode()
         msg = self.parser.parsestr(msg)
@@ -79,12 +99,13 @@ class PopServer(object):
 class EmailMonitor(object):
     def __init__(self, configurator):
         self.config = configurator
-        self.smtp_server = SmtpServer (configurator)
-        self.pop_server = PopServer (configurator)
-        self.cmd_executor = CmdExecutor ()
+        self.smtp_server = SmtpServer(configurator)
+        self.pop_server = PopServer(configurator)
+        self.cmd_executor = CmdExecutor()
         self.white_list = configurator.WHITE_LIST
-        self.msg_num = self.pop_server .msg_num
-        print('Initial Messages: %s' % self.msg_num)
+        self.msg_num = self.pop_server.msg_num
+        print('Initial Messages: %s' % self.msg_num, flush=True)
+
 
     def _decode_str(self, s):
         value, charset = decode_header(s)[0]
@@ -92,17 +113,19 @@ class EmailMonitor(object):
             value = value.decode(charset)
         return value
 
+
     def _decode_addr(self, addr):
         name, mailbox = parseaddr(addr)
         name = self._decode_str(name)
         return name, mailbox
 
+
     def handle_new_msgs(self, msg_num):
         for index in range(self.msg_num + 1, msg_num + 1):
             msg = self.pop_server.get_msg(index)
-            print(('Message: %d' % index).center(WIDTH, '-'))
             from_name, from_addr = self._decode_addr(msg.get('From'))
             subject = self._decode_str(msg.get('Subject', '')).strip()
+            print(('Message: %d' % index).center(WIDTH, '-'))
             print('From:', '%s <%s>' % (from_name, from_addr))
             print('Subject:', subject)
             if from_addr in self.white_list and self.cmd_executor.is_cmd_supported(subject):
@@ -135,6 +158,7 @@ class CmdExecutor(object):
 
     def is_cmd_supported(self, cmd_name):
         return cmd_name in self._commands
+
 
     def execute(self, cmd_name):
         if not self.is_cmd_supported(cmd_name):
